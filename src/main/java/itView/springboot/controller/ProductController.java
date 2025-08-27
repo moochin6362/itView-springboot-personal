2295735e4bf0e67b67c656e87e6c8a20639cfd3c
sakura0416710 <leejw1757@naver.com>
2025-08-27 12:43:31 +0900

    RuntimeException 수정

--- src/main/java/itView/springboot/controller/ProductController.java
+++ src/main/java/itView/springboot/controller/ProductController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import itView.springboot.exception.ProductException;
 import itView.springboot.service.ProductService;
 import itView.springboot.vo.User;
--- src/main/java/itView/springboot/exception/RuntimeException.java
+++
@@ -1,8 +0,0 @@
-package itView.springboot.exception;
-
-public class RuntimeException extends Exception {
-	public RuntimeException() {}
-	public RuntimeException(String msg) {
-		super(msg);
-	}
-}
