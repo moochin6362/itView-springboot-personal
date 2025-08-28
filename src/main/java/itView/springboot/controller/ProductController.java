8e188755a20fe0bd2892f16446baff5e52f1b186
ksh9714 <tkdgus9714@naver.com>
2025-08-28 09:57:38 +0900

    @SessionAttributes("loginUser") 추가

--- src/main/java/itView/springboot/controller/ProductController.java
+++ src/main/java/itView/springboot/controller/ProductController.java
@@ -3,10 +3,7 @@ package itView.springboot.controller;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 @Controller
 @RequestMapping({"/product", "/login", "/seller"})
 @RequiredArgsConstructor
+@SessionAttributes("loginUser")
 public class ProductController {
 	
 	private final ProductService pService;
