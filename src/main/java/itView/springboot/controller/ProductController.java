3a620be314bd3f70834c654ec7b430ecd6f7cf3e
ksh9714 <tkdgus9714@naver.com>
2025-09-09 22:27:53 +0900

    판매내역 페이지, css 및 체험단 관리 페이지, css

--- src/main/java/itView/springboot/controller/ProductController.java
+++ src/main/java/itView/springboot/controller/ProductController.java
@@ -131,7 +131,9 @@ public class ProductController {
 	// 판매 내역 페이지 이동
 	@GetMapping("mySellingPage")
 	public String mySellingPage() {
-		return "seller/mySelling";
+		
+		
+		return "seller/mySellingPage";
 	}
 	
 	// 내 상품 상세 페이지 이동
@@ -184,6 +186,12 @@ public class ProductController {
 		model.addAttribute("reviewCount", reviewCount);
 		
 		return "seller/myProductDetail";
+	}
+	
+	// 체험단 관리 
+	@GetMapping("experienceManagePage")
+	public String experienceManagePage() {
+		return "seller/experienceManagePage";
 	}
 	
 	// 상품 등록
@@ -336,7 +344,7 @@ public class ProductController {
 
 	// 상품 상세페이지
 	@GetMapping("/detail/{productNo}")
-	public String productdetail(@PathVariable int productNo, Model model) {
+	public String productdetail(@PathVariable("productNo") int productNo, Model model) {
 
 		return "Shopping/detail";
 	}
