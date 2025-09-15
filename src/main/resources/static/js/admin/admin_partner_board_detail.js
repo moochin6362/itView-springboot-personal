0f9f61a9c488595cd60fa5eb24f1ca3f4df1e77b
sakura0416710 <leejw1757@naver.com>
2025-09-15 10:20:03 +0900

    판매자문의게시판 댓글 등록js

--- src/main/resources/static/js/admin/admin_partner_board_detail.js
+++ src/main/resources/static/js/admin/admin_partner_board_detail.js
@@ -1,19 +1,24 @@
-//판매자 문의용으로 다시 바꾸기 
-
 function addReply(reply) {
-  const list = document.getElementById("replyList");
+  const list = document.getElementById("part-replyList");
   const itemWrapper = document.createElement("div");
-  itemWrapper.className = "genBoard-reply-list";
+  itemWrapper.className = "partBoard-reply-list";
+  
   const replyWriter = document.createElement("div");
-  replyWriter.className = "genBoard-reply-admin";
+  replyWriter.className = "partBoard-reply-admin";
   replyWriter.textContent = "관리자"; //관리자 답변은 이거로 고정
+  
   const replyDate = document.createElement("div");
-  replyDate.className = "genBoard-reply-date";
+  replyDate.className = "partBoard-reply-date";
+  
   const now = new Date();
   replyDate.textContent = now.toLocaleString(); //현재시간으로 추가
+  
+  
   const replyContent = document.createElement('div');
-  replyContent.className = "genBoard-reply-text";
+  replyContent.className = "partBoard-reply-text";
   replyContent.textContent = reply.replyContent;
+  
+  
   //작성자, 날짜, 내용 부분을 replyList div에 추가하기 (한묶음으로 추가하는 형식)
   itemWrapper.appendChild(replyWriter);
   itemWrapper.appendChild(replyDate);
@@ -22,12 +27,12 @@ function addReply(reply) {
   list.appendChild(itemWrapper);
 }
 
-document.getElementById("replySubmit").addEventListener("click", async () => {
-  const boardId = document.getElementById('gBoardDetail').value;
-  const content = document.getElementById("replyInput").value.trim();
+document.getElementById("part-replySubmit").addEventListener("click", async () => {
+  const boardId = document.getElementById('pBoardDetail').value;
+  const content = document.getElementById("part-replyInput").value.trim();
   if (!content) return alert("내용을 입력하세요.");
   try {
-    const res = await fetch(`/admin/gReply/${boardId}`, {
+    const res = await fetch(`/admin/pReply/${boardId}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ replyContent: content }),
@@ -35,6 +40,7 @@ document.getElementById("replySubmit").addEventListener("click", async () => {
     const newReply = await res.json();
     // 댓글 목록에 추가
     addReply(newReply);
+	
     // 입력창 초기화
     document.getElementById("replyInput").value = "";
   } catch (error) {
@@ -43,9 +49,11 @@ document.getElementById("replySubmit").addEventListener("click", async () => {
   }
 });
 
+
+
 //새로고침하면 댓글 달았던 것들이 사라져서 화면에 유지되도록!
 async function loadReplies(boardId) {
-  const res = await fetch(`/admin/gReplyList?boardId=${boardId}`);
+  const res = await fetch(`/admin/pReplyList?boardId=${boardId}`);
   if (!res.ok) return;
   const replyList = await res.json();
   const list = document.getElementById('replyList');
@@ -53,6 +61,6 @@ async function loadReplies(boardId) {
   replyList.forEach(reply => addReply(reply));
 }
 document.addEventListener("DOMContentLoaded", () => {
-  const boardId = document.getElementById('gBoardDetail').value;
+  const boardId = document.getElementById('pBoardDetail').value;
   loadReplies(boardId);
 });
